90f2bb8d1dd7d8c7f990f71aee00ffed3f9948cd
junlong <yaner6815@163.com>
2018-06-12 09:12:28 +0800

    jsYudu

---
+++ async/learn.js
@@ -0,0 +1,19 @@
+var message = '2'
+if(message){
+    console.log(message)
+}else{
+    console.log('bu')
+}
+console.log(07+0xA)
+var floatnum = 1.0
+var floatnum2 = 1.1
+console.log(floatnum,floatnum2)
+
+if (0.1 + 0.2 == 0.3){
+    console.log('0.1+0.2=0.3')
+}else{
+    console.log(0.1+0.2)//0.30000000000000004
+}
+
+console.log(isFinite(Number.MAX_VALUE+Number.MAX_VALUE))
+console.log(Number.MAX_VALUE)
