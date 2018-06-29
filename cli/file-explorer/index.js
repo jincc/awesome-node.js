2d24dee98afc8f2549b5d89c3a6dffeebe83c5e3
junlong <yaner6815@163.com>
2018-06-29 09:21:35 +0800

    c

--- cli/file-explorer/index.js
+++ cli/file-explorer/index.js
@@ -1,28 +1,32 @@
-var util = require('util')
-const EventEmitter = require('events')
-async function fn() {
-    // return 'hello world' /
-    // return Promise.reject(null)
-}
-const callBackFuntion = util.callbackify(fn)
-callBackFuntion((err,ret)=>{
-    // console.log(err.reason)
-    if (err) throw err;
-    console.log(ret)
+var fs = require('fs')
+process.stdout.write('    \033[31m  Enter your choic: /\033[39m')
+//process.cwd()当前路径
+fs.readdir(process.cwd(),function(err,files){
+    console.log(process.cwd())
+    if(!files.length){
+        return console.log('\033[31m No files to show!\033[39m\n')
+    }
+    console.log('Select which file or directory you want to see\n')
+
+    function file(i){
+        var fileName = files[i]
+        fs.stat(__dirname+'/'+fileName,function(err,stat){
+            if (stat.isDirectory()){
+                console.log('      '+i+'      \033[36m' + fileName + '/\033[39m')
+            }else{
+                console.log('      '+i+'      \033[90m' + fileName + '/\033[39m')
+            }
+            i++;
+            if(i == files.length){
+                console.log('')
+                process.stdout.write('    \033[31m  Enter your choic: /\033[39m')
+                //等待用户输入
+                process.stdin.resume()
+                process.stdin.setEncoding('utf8')
+            }else{
+                file(i)
+            }
+        })
+    }
+    file(0)
 })
-
-function MySteam(){
-    EventEmitter.call(this)
-}
-util.inherits(MySteam,EventEmitter)
-MySteam.prototype.write = function(data){
-    this.emit('data',data)
-}
-
-const stream = new MySteam()
-console.log(stream instanceof EventEmitter)
-console.log(MySteam.super_ === EventEmitter)
-
-
-var inspect = util.inspect(util,{showHidden:true,depth:null})
-console.log(inspect)
